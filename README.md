025b26858f668b2958e60d994550205e44f1f114
donzell <donzell@163.com>
2024-06-10 14:34:25 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Fast-TTS
+
+Fast-TTS 是一个基于异步框架的文本到语音转换（TTS）生成器项目。该项目利用了异步编程技术来高效处理请求和响应，实现了快速、秒级的流式生成长文本语音播放服务。Fast-TTS 可以快速地将长文本转换为语音流，并实时播放，适用于多种应用场景，如语音合成、智能助手、内容朗读等。
+
+## 特性
+
+- **异步处理**：使用异步框架和函数来高效处理HTTP请求。
+- **支持多种语言和声音**：通过微软TTS服务，支持多种语言和声音选项。
+- **简单易用的API**：提供简洁的API接口，便于集成和扩展。
+- **实时语音生成**：快速响应生成音频内容，适用于实时应用场景。
+
+## 截图
+
+### 主界面
+![Home Page](screenshots/home.png)
+
+## 安装
+
+1. 克隆仓库：
+    ```bash
+    git clone https://github.com/donzell888/fast-tts
+    cd TTS-Generator
+    ```
+
+2. 安装依赖：
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## 使用
+
+1. 启动服务：
+    ```bash
+    python server.py
+    ```
+
+2. 访问主页：
+    打开浏览器并访问 `http://127.0.0.1:5000/`。
+
+3. API端点：
+
+    - **生成TTS音频**：
+        ```http
+        POST /generate
+        ```
+
+        **请求体**：文本数据（将转换为语音）
+        
+        **响应**：包含音频URL的JSON对象
+## API示例
+
+### POST /generate
+
+请求示例：
+```http
+POST /generate HTTP/1.1
+Host: 127.0.0.1:5000
+Content-Type: application/json
+
+{
+    "content": "你好，世界！",
+    "voice": "zh-CN-XiaoxiaoNeural"
+}
---
+++ server.py
@@ -0,0 +1,42 @@
+import binascii
+from quart import Quart, request, jsonify, Response, render_template
+import json
+import edge_tts
+
+app = Quart(__name__)
+
+
+@app.route('/')
+async def home():
+    return await render_template('home.html')
+
+
+@app.route('/generate', methods=['POST'])
+async def generate():
+    data = await request.get_data()
+    payload = binascii.hexlify(data).decode('utf8')
+    t = {"code": 1, "audio_url": "/tts?payload=" + payload}
+    return jsonify(t)
+
+
+@app.route('/tts', methods=['GET'])
+async def tts():
+    payload = request.args.get("payload")
+    plaintext = binascii.unhexlify(payload).decode('utf8')
+    json_obj = json.loads(plaintext)
+    content = json_obj["content"]
+    voice = json_obj.get("voice", "zh-CN-XiaoxiaoNeural")
+    headers = {'Transfer-Encoding': 'chunked', 'Content-Type': 'audio/webm'}
+    response_content = ms_tts_async(content, voice)
+    return Response(response_content, headers=headers)
+
+
+async def ms_tts_async(text_input, voice="zh-CN-XiaoxiaoNeural"):
+    communicate = edge_tts.Communicate(text_input, voice)
+    async for chunked in communicate.stream():
+        if chunked["type"] == "audio":
+            yield chunked["data"]
+
+
+if __name__ == '__main__':
+    app.run()
